23e7d196a07a91afb93bdd521423cbdbf5f5f2c4
changdada188 <1067720919@qq.com>
2023-05-01 23:26:25 +0800

    Add: draft

---
+++ lua/plugin-config/lualine.lua
@@ -0,0 +1,65 @@
+-- 如果找不到lualine 组件，就不继续执行
+local status, lualine = pcall(require, "lualine")
+if not status then
+    vim.notify("没有找到 lualine")
+  return
+end
+
+lualine.setup({
+  options = {
+    icons_enabled = true,
+    theme = "Tomorrow",
+    --component_separators = { left = "|", right = "|" },
+    component_separators = { left = '', right = ''},
+    -- https://github.com/ryanoasis/powerline-extra-symbols
+    section_separators = { left = " ", right = "" },
+  },
+  extensions = { "nvim-tree", "toggleterm" },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {
+      "filename",
+      {
+        "lsp_progress",
+        spinner_symbols = { " ", " ", " ", " ", " ", " " },
+      },
+    },
+    lualine_x = {
+      "filesize",
+      {
+        "fileformat",
+        symbols = {
+          unix = "LF",
+          dos = "CRLF",
+          mac = "CR",
+        },
+      },
+      "encoding",
+      "filetype",
+    },
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {'location'}
+  },
+
+--  winbar = {
+--  lualine_a = {},
+--  lualine_b = {},
+--  lualine_c = {'filename'},
+--  lualine_x = {},
+--  lualine_y = {},
+--  lualine_z = {}
+--  },
+
+
+})
+
---
+++ lua/plugins.lua
@@ -0,0 +1,86 @@
+-- 自动安装 Packer.nvim
+-- 插件安装目录
+-- ~/.local/share/nvim/site/pack/packer/
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local paccker_bootstrap
+if fn.empty(fn.glob(install_path)) > 0 then
+    vim.notify("正在安装Pakcer.nvim，请稍后...")
+    paccker_bootstrap = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        -- "https://gitcode.net/mirrors/wbthomason/packer.nvim",
+        install_path,
+    })
+
+    -- https://github.com/wbthomason/packer.nvim/issues/750
+    local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
+    if not string.find(vim.o.runtimepath, rtp_addition) then
+        vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
+    end
+    vim.notify("Pakcer.nvim 安装完毕")
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    vim.notify("没有安装 packer.nvim")
+    return
+end
+
+packer.startup({
+    function(use)
+        -- Packer 可以升级自己
+        use("wbthomason/packer.nvim")
+        -------------------------- plugins -------------------------------------------
+        -- nvim-tree
+        use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+        -- bufferline
+        use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" } })
+        -- lualine
+        use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
+        use("arkav/lualine-lsp-progress")
+        -- telescope
+        use({ 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" } })
+        -- telescope extensions
+        use("LinArcX/telescope-env.nvim")
+        -- dashboard-nvim
+        -- use({'glepnir/dashboard-nvim', requires = {"kyazdani42/nvim-web-devicons"}})
+        -- project
+        -- use("ahmedkhalf/project.nvim")
+        -- treesitter
+        use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+        --------------------- LSP --------------------
+        use("williamboman/nvim-lsp-installer")
+        -- Lspconfig
+        use({ "neovim/nvim-lspconfig" })
+
+        -- 补全引擎
+        use("hrsh7th/nvim-cmp")
+        -- snippet 引擎
+        use("hrsh7th/vim-vsnip")
+        -- 补全源
+        use("hrsh7th/cmp-vsnip")
+        use("hrsh7th/cmp-nvim-lsp") -- { name = nvim_lsp }
+        use("hrsh7th/cmp-buffer")   -- { name = 'buffer' },
+        use("hrsh7th/cmp-path")     -- { name = 'path' }
+        use("hrsh7th/cmp-cmdline")  -- { name = 'cmdline' }
+        -- 常见编程语言代码段
+        use("rafamadriz/friendly-snippets")
+        -- indent-blankline
+        use("lukas-reineke/indent-blankline.nvim")
+        -- vimspector
+        use("puremourning/vimspector")
+
+        -- Comment
+        use("numToStr/Comment.nvim")
+
+        if paccker_bootstrap then
+            packer.sync()
+        end
+    end,
+
+})
