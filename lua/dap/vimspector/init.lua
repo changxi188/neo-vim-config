d102e441da3d70d455e964362ecf11546f5d74e7
贝叶斯-成昌喜 <chengchangxi@holomatic.com>
2024-05-25 23:09:51 +0800

    Add: custom vimspector ui

--- lua/dap/vimspector/init.lua
+++ lua/dap/vimspector/init.lua
@@ -11,31 +11,45 @@ require("keybindings").mapVimspector()
 -- 设置基本尺寸
 vim.g.vimspector_sidebar_width = 80
 vim.g.vimspector_code_minwidth = 120
-vim.g.vimspector_terminal_minwidth = 90
-vim.g.vimspector_terminal_height = 20
+vim.g.vimspector_code_maxwidth = 120
+vim.g.vimspector_terminal_minwidth = 120
+vim.g.vimspector_terminal_maxwidth = 120
+-- vim.g.vimspector_terminal_height = 20
 vim.g.vimspector_variables_display_mode = 'full'
--- vim.g.vimspector_bottombar_height = 34
 
 -- 自定义UI函数
 local function customise_ui()
-  vim.fn.win_gotoid(vim.g.vimspector_session_windows.variables)
-  vim.cmd('resize 1')
   -- 切换到输出窗口并关闭它
   vim.fn.win_gotoid(vim.g.vimspector_session_windows.output)
   vim.cmd('q')
 
-  vim.fn.win_gotoid(vim.g.vimspector_session_windows.terminal)
-  vim.cmd('set norelativenumber nonumber')
+
+  vim.fn.win_gotoid(vim.g.vimspector_session_windows.code)
   vim.cmd('resize 35')
+  vim.cmd('set wfh')
+  vim.cmd('set wfw')
+  --保持窗口大小不变
+  vim.fn.win_gotoid(vim.g.vimspector_session_windows.terminal)
 end
 
 -- 设置终端窗口的函数
 local function setup_terminal()
+  vim.fn.win_gotoid(vim.g.vimspector_session_windows.variables)
+  vim.cmd('resize 12')
+  vim.fn.win_gotoid(vim.g.vimspector_session_windows.watches)
+  vim.cmd('vertical resize 70')
+  vim.cmd('set wfh')
+  vim.cmd('set wfw')
 
+  vim.fn.win_gotoid(vim.g.vimspector_session_windows.stack_trace)
+  vim.cmd('vertical resize 70')
+  vim.cmd('resize 12')
+  vim.cmd('set wfh')
+  vim.cmd('set wfw')
   -- 切换到终端窗口并禁用行号
   vim.fn.win_gotoid(vim.g.vimspector_session_windows.terminal)
   vim.cmd('set norelativenumber nonumber')
-  vim.cmd('resize 35')
+  -- vim.cmd('resize 35')
 
 end
 
